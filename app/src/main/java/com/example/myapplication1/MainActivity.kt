16a2f6b403bdd936eb48edfce890ad81d64490e2
HARINESH <harineshselvakumar2006@gmail.com>
2024-06-23 11:34:00 +0530

    additrmactivity

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication1
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,10 +16,15 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.myapplication1.Additem
 import com.example.myapplication1.ui.theme.MyApplication1Theme
 
+
+data class Starter(val name:String)
+data class Maincourse(val name: String)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,14 +37,19 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainContent()
-{
+{ val context= LocalContext.current
+    val activity=context as? Activity
     Box(modifier = Modifier.fillMaxSize())
     {
         TabLayout()
 
-    FloatingActionButton(onClick = { /*TODO*/ },
+    FloatingActionButton(onClick = {
+                                   context.startActivity(Intent(context, Additem::class.java))
+        activity?.finish()
+    },
         containerColor = Color.Yellow,
         modifier = Modifier
             .align(Alignment.BottomEnd)
@@ -48,7 +60,7 @@ fun MainContent()
 @Composable
 fun TabLayout() {
     var selectedTabIndex by remember { mutableStateOf(0) }
-    val tabs = listOf("Tab 1", "Tab 2", "Tab 3")
+    val tabs = listOf("Food", "FunScreen")
 
     Column(modifier = Modifier.fillMaxSize()
     ) {
@@ -66,45 +78,12 @@ fun TabLayout() {
                 )
             }
         }
-        when (selectedTabIndex) {
-            0 -> Tabcontent(content = "Content for Tab 1")
-            1 -> Tabcontent(content = "Content for Tab 2")
-            2 -> Tabcontent(content = "Content for Tab 3")
-        }
+
     }
 }
 
-@Composable
-fun Tabcontent(content: String) {
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(1.dp)
-    ) {
-        Text(text = content)
-    }
-}
-@Composable
-fun Buut()
-{
-Column(
-    modifier = Modifier.fillMaxSize(),
-    horizontalAlignment = Alignment.End,
-    verticalArrangement = Arrangement.Bottom
-)
-{
-    Button(onClick = { /*TODO*/ },
-            modifier = Modifier
-                .padding(16.dp)
-                .height(48.dp)
-                .background(Color.Gray)
-                .width(200.dp),
-        shape = RoundedCornerShape(16.dp),
-    ) {
-        Text(text = "+")
 
 
-}}}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
